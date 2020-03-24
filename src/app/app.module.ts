46b9f17766cc20053d31e58577f9858a3b747e11
LukasFrmm <lukasfreimonas@gmail.com>
2020-03-24 07:38:03 +0200

    summary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { CalendarDayComponent } from './components/calendar-day/calendar-day.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BottomComponent } from './components/bottom/bottom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
-    CalendarDayComponent
+    CalendarDayComponent,
+    SummaryComponent,
+    BottomComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
